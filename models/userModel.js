1f0f93eb06421f4eabc839a5a6e41624071cbab6
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-04-11 04:58:05 -0300

    hash password

--- models/userModel.js
+++ models/userModel.js
@@ -30,6 +30,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function(next){
     const salt = await bcrypt.genSaltSync(10);
+    this.password = await bcrypt.hash(this.password,salt);
 })
 
 //Export the model
