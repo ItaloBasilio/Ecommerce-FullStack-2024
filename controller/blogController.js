89e08f6279fbcde43816f16d47f933cb95376403
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-04-18 02:59:02 -0300

    update

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,11 +1,11 @@
 const Blog = require('../models/blogModel');
 const User = require('../models/userModel');
-const asynchandler = require('express-async-handler');
+const asyncHandler = require('express-async-handler');
 const validateMongoDbId = require('../utils/validateMongodbId');
 
 // Create Blog
 
-const createBlog = asynchandler(async(req,res) => {
+const createBlog = asyncHandler(async(req,res) => {
     try {
         const newBlog = await Blog.create(req.body);
         res.json(newBlog);
@@ -16,4 +16,67 @@ const createBlog = asynchandler(async(req,res) => {
 
 })
 
-module.exports = { createBlog };
+// Update Blog
+
+const updateBlog = asyncHandler(async(req,res) => {
+    const { id } = req.params;
+    try {
+        const updateBlog = await Blog.findByIdAndUpdate(id, req.body,{ 
+            new:true 
+        });
+        res.json(updateBlog);
+    } catch (error) {
+        throw new Error(error)   
+    }
+
+})
+
+// Get blog
+
+const getBlog = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try{
+        const getBlog = await Blog.findById(id);
+        const updateViews = await Blog.findByIdAndUpdate(
+            id,
+            {
+                $inc: {numViews: 1},
+            },
+            { new: true}
+        );
+        res.json(updateViews);
+    }catch(error){
+        throw new Error(error);
+    }
+    
+});
+
+// get All Blog
+
+const getAllBlogs = asyncHandler(async (req, res) => {
+    try {
+        const blogs = await Blog.find({});
+        res.json(blogs);
+    } catch (error) {
+        res.status(500);
+        throw new Error('Internal server error');
+    }
+});
+
+
+// Delete Blog
+
+const deleteBlogById = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+  
+    const blog = await Blog.findByIdAndDelete(id);
+  
+    if (!blog) {
+      res.status(404);
+      throw new Error('Blog not found');
+    }
+  
+    res.json({ message: 'Blog removed' });
+  });
+
+module.exports = { createBlog , updateBlog, getBlog , getAllBlogs , deleteBlogById  };
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
-const { createBlog } = require('../controller/blogController');
+const { createBlog, updateBlog, getBlog ,getAllBlogs, deleteBlogById,  } = require('../controller/blogController');
 const router = express.Router();
 
 router.post('/', authMiddleware, isAdmin, createBlog);
-
+router.put('/:id', authMiddleware, isAdmin, updateBlog);
+router.get('/:id',authMiddleware, isAdmin, getBlog)
+router.get('/',authMiddleware, isAdmin, getAllBlogs);
+router.delete('/:id',authMiddleware, isAdmin, deleteBlogById )
 
 module.exports = router ;
