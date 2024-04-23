4d3ad2b60f27b0a7fd4422f924b15d0ca7080752
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-04-23 11:01:20 -0300

    update

--- controller/userController.js
+++ controller/userController.js
@@ -4,6 +4,7 @@ const Product = require('../models/productModel');
 const Cart = require('../models/cartModel');
 const Coupon = require('../models/couponModel');
 const Order = require('../models/orderModel');
+const uniqid = require('uniqid');
 
 const asyncHandler = require('express-async-handler');
 const validateMongodbId = require('../utils/validateMongodbId');
@@ -410,7 +411,7 @@ const applyCoupon = asyncHandler(async (req, res) => {
     }
     const user = await User.findOne({ _id });
     const cart = await Cart.findOne({ orderby: user._id }).populate('products.product');
-    
+
     if (!cart) {
         throw new Error("Cart not found");
     }
@@ -427,8 +428,92 @@ const applyCoupon = asyncHandler(async (req, res) => {
     res.json(totalAfterDiscount);
 });
 
+// Criar ordens
+
+const createOrder = asyncHandler(async (req, res) => {
+    const { COD, couponApplied } = req.body;
+    const { _id } = req.user;
+    validateMongodbId(_id);
+    try {
+        if (!COD) {
+            throw new Error('Create cash order failed');
+        }
+        const user = await User.findById(_id);
+        let userCart = await Cart.findOne({ orderby: user._id });
+        let finalAmount = 0;
+
+        if (couponApplied && userCart.totalAfterDiscount) {
+            finalAmount = userCart.totalAfterDiscount;
+        } else {
+            finalAmount = userCart.cartTotal;
+        }
+
+        let newOrder = await new Order({
+            products: userCart.products,
+            paymentIntent: {
+                id: uniqid(),
+                method: "COD",
+                amount: finalAmount,
+                status: "Cash on Delivery",
+                created: Date.now(),
+                currency: "brl",
+            },
+            orderby: user._id,
+            orderStatus: "Cash on Delivery",
+        }).save();
+        let update = userCart.products.map((item) => {
+            return {
+                updateOne: {
+                    filter: { _id: item.product._id },
+                    update: { $inc: { quantity: -item.count, sold: +item.count } },
+                }
+            }
+        })
+        const updated = await Product.bulkWrite(update, {});
+        res.json({ message: "success" })
+    } catch (error) {
+        throw new Error(error);
+    }
+});
 
 
+// get Orders
+
+const getOrders = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    validateMongodbId(_id);
+    try {
+        const userOrders = await Order.find({ orderby: _id }).populate('products.product').exec();
+        res.json(userOrders);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+//update orders
+
+const updateOrderStatus = asyncHandler(async (req, res) => {
+    const { status } = req.body;
+    const { id} = req.params;
+    validateMongodbId(id);
+    try {
+        const updateOrderStatus = await Order.findByIdAndUpdate(
+            id,
+            {
+                orderStatus: status,
+                paymentIntent: {
+                    status: status,
+                }
+
+            },
+
+            { new: true }
+        );
+        res.json(updateOrderStatus)
+    } catch (error) {
+        throw new Error(error);
+    }
+})
 
 
 
@@ -452,5 +537,8 @@ module.exports = {
     userCart,
     getUserCart,
     emptyCart,
-    applyCoupon
+    applyCoupon,
+    createOrder,
+    getOrders,
+    updateOrderStatus
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -19,22 +19,28 @@ const {
     userCart,
     getUserCart,
     emptyCart,
-    applyCoupon
+    applyCoupon,
+    createOrder,
+    getOrders,
+    updateOrderStatus
 } = require('../controller/userController');
 const {authMiddleware , isAdmin} = require('../middlewares/authMiddleware');
 const router = express.Router();
 router.post('/register', createUser);
 router.post('/forgot-password-token', forgotPasswordToken);
-
 router.put('/reset-password/:token', resetPassword);
-router.put('/password', authMiddleware, updatePassword)
+router.put('/order/update-order/:id', authMiddleware, isAdmin, updateOrderStatus);
 
+router.put('/password', authMiddleware, updatePassword)
 router.post('/login', loginUserControl);
 router.post('/admin-login', loginAdmin);
 router.post('/cart', authMiddleware, userCart);
 router.post('/cart/applycoupon', authMiddleware, applyCoupon)
+router.post('/cart/cash-order', authMiddleware, createOrder)
+
 
 router.get('/all-users', getAllUser);
+router.get('/get-orders', authMiddleware, getOrders)
 router.get('/refresh', handleRefreshToken );
 router.get('/logout', logout );
 
