917d38c5d46618f1ac47aa416c48ec1ccc59396d
Italo Basilio da Silva <italobasilio@gmail.com>
2024-05-24 05:31:31 -0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 // COMPONENTES
 
@@ -13,6 +15,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout/>}>
             <Route index element={<Home/>}/>
+            <Route path="about" element={<About/>}/>
+            <Route path="contact" element={<Contact/>}/>
+
           </Route>
         </Routes>
       </BrowserRouter>
