84f1406e65ba58c034aac9f44d5fd53894831787
Italo Basilio da Silva <italobasilio@gmail.com>
2024-05-27 00:31:13 -0300

    update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,25 @@
 import React from 'react'
-import { NavLink,Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
   return <>
-  <header className="header-top-strip">
-    <div className="container-xxl">
-      <div className="row">
-        <div className="col-6"></div>
-        <div className="col-6"></div>
+    <header className="header-top-strip py-3">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-6">
+            <p className='text-white mb-0'>
+              Frete Grátis acima R$100 & Devolução Grátis
+            </p>
+          </div>
+          <div className="col-6">
+            <p className='text-end text-white mb-0'>
+              Suporte:
+              <a className='text-white' href="tel: (19)1234 5678">(19)1234 5678</a>
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-  </header>
+    </header>
   </>;
 }
 
