ae7624dd1b161b215d562efbf19992f280b95222
Italo Basilio da Silva <italobasilio@gmail.com>
2024-05-27 00:13:19 -0300

    update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { NavLink,Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+  return <>
+  <header className="header-top-strip">
+    <div className="container-xxl">
+      <div className="row">
+        <div className="col-6"></div>
+        <div className="col-6"></div>
+      </div>
+    </div>
+  </header>
+  </>;
 }
 
 export default Header
